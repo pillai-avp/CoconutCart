1041cf10688a5efc971099121fea67b2ddd05a68
arunpillai <arun.vijayan.pillai@ruter.no>
2022-03-23 14:12:31 +0100

    sync data to and from remote server

--- api/src/main/java/net/insi8/coconut/api/datasource/GroceriesDataSource.kt
+++ api/src/main/java/net/insi8/coconut/api/datasource/GroceriesDataSource.kt
@@ -3,13 +3,14 @@ package net.insi8.coconut.api.datasource
 import kotlinx.coroutines.delay
 import net.insi8.coconut.api.CoconutAPI
 import net.insi8.coconut.api.data.Cart
+import net.insi8.coconut.api.data.Item
 import net.insi8.coconut.api.data.common.Result
 import net.insi8.coconut.api.extensions.toResult
 import retrofit2.awaitResponse
 
 interface GroceriesCartDataSource {
     suspend fun getGroceriesCart(): Result<Cart>
-    suspend fun updateCart(productId: Long, updatedQuantity: Long): Result<Unit>
+    suspend fun updateCart(cartItems: Map<Long, Long>): Result<List<Item>>
 }
 
 class GroceriesCartRepository(private val coconutAPI: CoconutAPI) : GroceriesCartDataSource {
@@ -17,8 +18,8 @@ class GroceriesCartRepository(private val coconutAPI: CoconutAPI) : GroceriesCar
         return coconutAPI.getCart().awaitResponse().toResult()
     }
 
-    override suspend fun updateCart(productId: Long, updatedQuantity: Long): Result<Unit> {
+    override suspend fun updateCart(cartItems: Map<Long, Long>): Result<List<Item>> {
         delay(800L)
-        return Result.withEmptySuccess()
+        return Result.successIfNotNull(emptyList<Item>())
     }
 }
--- app/src/main/java/net/insi8/coconut/cart/GroceriesCartUseCase.kt
+++ app/src/main/java/net/insi8/coconut/cart/GroceriesCartUseCase.kt
@@ -25,21 +25,40 @@ class GroceriesCartUseCase(
             cart
         }
         cart?.let {
-            saveLocalStorage(it)
+            syncLocalData(it)
         }
     }
 
-    private suspend fun saveLocalStorage(cart: Cart) {
-        val carts =
+    private suspend fun syncLocalData(cart: Cart) {
+        val networkCartItems =
             cart.items.map { CartEntity(product_id = it.product.id, quantity = it.quantity) }
-        carts.forEach {
+        val localCartItems = cartDao.gelAllCartItems()
+        val syncedLocalCartItems = localCartItems.filter { it.synced }
+        val notSyncedLocalCartItems = localCartItems.filterNot { it.synced }
+
+        // new from server
+        networkCartItems.filterNot { networkItems ->
+            localCartItems.map { it.product_id }.contains(networkItems.product_id)
+        }.map {
             cartDao.insertItem(item = it)
         }
+        // update synced from network
+        networkCartItems.filter { networkItems ->
+            syncedLocalCartItems.map { it.product_id }.contains(networkItems.product_id)
+        }.map {
+            cartDao.updateItem(cart = it)
+        }
+        // Update the remote cart
+        updateRemoteCart(cartItems = mutableMapOf<Long, Long>().apply {
+            notSyncedLocalCartItems.forEach {
+                this[it.product_id] = it.quantity
+            }
+        })
     }
 
     suspend fun updateCartItem(productId: Long, updatedQuantity: Long) {
         updateLocalStorage(productId, updatedQuantity)
-        syncUpdatedCart(productId, updatedQuantity)
+        updateRemoteCart(mutableMapOf((productId to updatedQuantity)))
     }
 
     private suspend fun updateLocalStorage(productId: Long, updatedQuantity: Long) {
@@ -50,36 +69,39 @@ class GroceriesCartUseCase(
                 synced = false
             )
         )
-        updateCartState(productId, updatedQuantity, false)
+        updateCartState(mutableMapOf((productId to updatedQuantity)), false)
     }
 
-    private suspend fun syncUpdatedCart(productId: Long, updatedQuantity: Long) {
+    private suspend fun updateRemoteCart(cartItems: Map<Long, Long>) {
         coroutineScope {
             launch {
-                val result = dataSource.updateCart(productId, updatedQuantity)
-                result.asSuccessOrNull()?.let {
+                val result = dataSource.updateCart(cartItems = cartItems)
+                result.asSuccessOrNull()?.forEach { it ->
                     cartDao.updateItem(
                         CartEntity(
-                            product_id = productId,
-                            quantity = updatedQuantity,
+                            product_id = it.product.id,
+                            quantity = it.quantity,
                             synced = true
                         )
                     )
                 }
-                updateCartState(productId, updatedQuantity, true)
+                updateCartState(cartItems, true)
             }
         }
     }
 
-    private fun updateCartState(productId: Long, updatedQuantity: Long, synced: Boolean) {
+    private fun updateCartState(cartItems: Map<Long, Long>, synced: Boolean) {
         _cartState.update { state ->
+            val updatedCartItemIds = cartItems.keys
             state?.let { cart ->
                 cart.copy(items = cart.items.mapNotNull { item ->
-                    if (item.product.id == productId) {
-                        if (synced && updatedQuantity == 0L) {
+                    if (updatedCartItemIds.contains(item.product.id)) {
+                        if (synced && cartItems[item.product.id] == 0L) {
                             null
                         } else {
-                            item.copy(quantity = updatedQuantity)
+                            cartItems[item.product.id]?.let {
+                                item.copy(quantity = it)
+                            }
                         }
                     } else {
                         item
